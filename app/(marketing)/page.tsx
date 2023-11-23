8f81c26341b06a819a5cd67b3c00ce7030f4171d
Zaid-maker <pzhafeez@gmail.com>
2023-11-23 23:01:09 +0500

    add latin font on description

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,9 +4,15 @@ import Link from "next/link";
 import React from "react";
 import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
 
 const headingFont = localFont({
   src: "../../public/fonts/font.woff2",
+});
+
+const textFont = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 const MarketingPage = () => {
@@ -29,7 +35,12 @@ const MarketingPage = () => {
           work forward.
         </div>
       </div>
-      <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+      <div
+        className={cn(
+          "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+          textFont.className
+        )}
+      >
         Collaborate, manage projects, and reach new productivity peaks. From
         high rises to the home office, the way your team works is unique -
         accomplish it all with Taskify.
