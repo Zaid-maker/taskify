eeec2647e3e0a9fa886a0dd9d7ab0c620355d98c
Zaid-maker <pzhafeez@gmail.com>
2023-11-23 22:45:49 +0500

    add a button

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { Medal } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const MarketingPage = () => {
@@ -21,6 +23,9 @@ const MarketingPage = () => {
         high rises to the home office, the way your team works is unique -
         accomplish it all with Taskify.
       </div>
+      <Button className="mt-6" size="lg" asChild>
+        <Link href="/sign-up">Get Taskify for free</Link>
+      </Button>
     </div>
   );
 };
