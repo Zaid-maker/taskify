428c10ccb56d052a7607eb9c3ab58db242a143db
Zaid-maker <pzhafeez@gmail.com>
2023-11-23 22:06:18 +0500

    update

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -9,6 +9,12 @@ const MarketingPage = () => {
           <Medal className="h-6 w-6 mr-2" />
           No 1 task managment
         </div>
+        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
+          Taskify helps team move
+        </h1>
+        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+          work forward.
+        </div>
       </div>
     </div>
   );
