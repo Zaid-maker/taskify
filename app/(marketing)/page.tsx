5262f93ca69f448933ddefe0570fcc281cdf3f6c
Zaid-maker <pzhafeez@gmail.com>
2023-11-23 21:50:22 +0500

    add missing style

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,7 +5,7 @@ const MarketingPage = () => {
   return (
     <div className="flex items-center justify-center flex-col">
       <div className="flex items-center justify-center flex-col">
-        <div className="mb-4 flex items-center border shadow-sm p-4">
+        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
           No 1 task managment
         </div>
