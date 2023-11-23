c69a10a68392c20297462260d07f58e6a27b41c5
Zaid-maker <pzhafeez@gmail.com>
2023-11-23 22:54:35 +0500

    add custom font on heading

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,11 +2,21 @@ import { Button } from "@/components/ui/button";
 import { Medal } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import localFont from "next/font/local";
+import { cn } from "@/lib/utils";
 
+const headingFont = localFont({
+  src: "../../public/fonts/font.woff2",
+});
 const MarketingPage = () => {
   return (
     <div className="flex items-center justify-center flex-col">
-      <div className="flex items-center justify-center flex-col">
+      <div
+        className={cn(
+          "flex items-center justify-center flex-col",
+          headingFont.className
+        )}
+      >
         <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
           No 1 task managment
