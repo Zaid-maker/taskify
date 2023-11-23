fc1222ba65b1baa60cf419f93089a9e8a93b5825
Zaid-maker <pzhafeez@gmail.com>
2023-11-23 21:47:58 +0500

    update the page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,17 @@
+import { Medal } from "lucide-react";
 import React from "react";
 
 const MarketingPage = () => {
-  return <div>MarketingPage</div>;
+  return (
+    <div className="flex items-center justify-center flex-col">
+      <div className="flex items-center justify-center flex-col">
+        <div className="mb-4 flex items-center border shadow-sm p-4">
+          <Medal className="h-6 w-6 mr-2" />
+          No 1 task managment
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MarketingPage;
