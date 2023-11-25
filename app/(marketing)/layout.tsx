9356ce4c6e48ac68eef7922e636059180845bcf8
Zaid-maker <pzhafeez@gmail.com>
2023-11-25 22:50:48 +0500

    create Footer component

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Footer } from "./_components/footer";
 import { Navbar } from "./_components/navbar";
 
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
@@ -6,6 +7,7 @@ const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
     <div className="h-full bg-slate-100">
       <Navbar />
       <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      <Footer />
     </div>
   );
 };
