38ed96293348823a9ea1a3159457e1600ccfb055
Zaid-maker <pzhafeez@gmail.com>
2023-11-25 20:59:55 +0500

    update navbar

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,5 +1,12 @@
+import { Logo } from "@/components/logo";
 import React from "react";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
+      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        <Logo />
+      </div>
+    </div>
+  );
 };
