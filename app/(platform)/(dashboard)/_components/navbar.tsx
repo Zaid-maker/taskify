a33e713b007a0560bca32d669af97aac82a8d12d
Zaid-maker <pzhafeez@gmail.com>
2023-11-27 17:22:16 +0500

    create layout for dashboard

---
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
+      Navbar
+    </div>
+  );
+};
