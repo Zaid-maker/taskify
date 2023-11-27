4f2b201fb40c22da5175fae5a3f8e4f05d2e3db2
Zaid-maker <pzhafeez@gmail.com>
2023-11-27 17:24:05 +0500

    add logo to the navbar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,9 +1,14 @@
+import { Logo } from "@/components/logo";
 import React from "react";
 
 export const Navbar = () => {
   return (
-    <div className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
-      Navbar
-    </div>
+    <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
+      <div className="flex items-center gap-x-4">
+        <div className="hidden md:flex">
+          <Logo />
+        </div>
+      </div>
+    </nav>
   );
 };
