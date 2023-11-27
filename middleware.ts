59b06af984b5d4eba398731991482c38bed53011
Zaid-maker <pzhafeez@gmail.com>
2023-11-27 21:11:57 +0500

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,33 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
-    publicRoutes: ['/', "/api/webhook"]
+    publicRoutes: ["/", "/api/webhook"],
+    afterAuth(auth, req) {
+        if (auth.userId && auth.isPublicRoute) {
+            let path = "/select-org";
+
+            if (auth.orgId) {
+                path = `/organization/${auth.orgId}`;
+            }
+
+            const orgSelection = new URL(path, req.url);
+
+            return NextResponse.redirect(orgSelection);
+        }
+
+        if (!auth.userId && !auth.isPublicRoute) {
+            return redirectToSignIn({ returnBackUrl: req.url });
+        }
+
+        if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+            const orgSelection = new URL("/select-org", req.url);
+
+            return NextResponse.redirect(orgSelection);
+        }
+    },
 });
 
 export const config = {
-    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
