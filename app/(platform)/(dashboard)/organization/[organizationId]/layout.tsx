452803a422617f34af739d2e6bf9e06ff415c571
Zaid-maker <pzhafeez@gmail.com>
2023-11-28 00:43:19 +0500

    add layout

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,21 @@
+import { auth } from "@clerk/nextjs";
+import React from "react";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
+
+const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      {/* <OrgControl /> */}
+      {children}
+    </>
+  );
+};
+
+export default OrganizationIdLayout;
