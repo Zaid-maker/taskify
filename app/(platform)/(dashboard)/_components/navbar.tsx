9248f41f89ec1af5039c4207e73bc1f37b344c95
Zaid-maker <pzhafeez@gmail.com>
2023-11-27 17:28:00 +0500

    update dashboard navbar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,4 +1,5 @@
 import { Logo } from "@/components/logo";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 export const Navbar = () => {
@@ -8,6 +9,34 @@ export const Navbar = () => {
         <div className="hidden md:flex">
           <Logo />
         </div>
+      </div>
+      <div className="ml-auto flex items-center gap-x-2">
+        <OrganizationSwitcher
+          hidePersonal
+          afterCreateOrganizationUrl="/organization/:id"
+          afterLeaveOrganizationUrl="/select-org"
+          afterSelectOrganizationUrl="/organization/:id"
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              },
+            },
+          }}
+        />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: {
+                height: 30,
+                width: 30,
+              },
+            },
+          }}
+        />
       </div>
     </nav>
   );
