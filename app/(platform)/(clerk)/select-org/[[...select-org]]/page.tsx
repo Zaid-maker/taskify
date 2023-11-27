3609ddd2fdc0ee0adb2d7317ba7045ae678dc105
Zaid-maker <pzhafeez@gmail.com>
2023-11-27 16:57:03 +0500

    update

--- app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -1,5 +1,11 @@
 import { OrganizationList } from "@clerk/nextjs";
 
 export default function CreateOrganizationPage() {
-  return <OrganizationList />;
+  return (
+    <OrganizationList
+      hidePersonal
+      afterSelectOrganizationUrl="/organization/:id"
+      afterCreateOrganizationUrl="/organization/:id"
+    />
+  );
 }
