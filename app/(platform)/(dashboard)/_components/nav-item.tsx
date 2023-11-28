f9fa6bf3565ece9e9f86a7b0e9a56ccf139e6e70
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-11-28 11:33:27 +0000

    update

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -24,3 +24,14 @@ export const NavItem = ({
 }: NavItemProps) => {
   return <div>NavItem</div>;
 };
+
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <div className="w-10 h-10 relative shrink-0">
+        <Skeleton className="h-full w-full absolute" />
+      </div>
+      <Skeleton className="h-10 w-full" />
+    </div>
+  );
+};
