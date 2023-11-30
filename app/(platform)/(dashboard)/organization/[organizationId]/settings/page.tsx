51820057cc8118e9a02452b3f8f002fe92141c4d
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-11-30 20:21:25 +0000

    complete

--- app/(platform)/(dashboard)/organization/[organizationId]/settings/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/settings/page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { OrganizationProfile } from "@clerk/nextjs";
 
 const SettingsPage = () => {
-  return <div>SettingsPage</div>;
+  return (
+    <div className="w-full">
+      <OrganizationProfile
+        appearance={{
+          elements: {
+            rootBox: {
+              boxShadow: "none",
+              width: "100%",
+            },
+            card: {
+              border: "1px solid #e5e5e5",
+              boxShadow: "none",
+              width: "100%",
+            },
+          },
+        }}
+      />
+    </div>
+  );
 };
 
 export default SettingsPage;
