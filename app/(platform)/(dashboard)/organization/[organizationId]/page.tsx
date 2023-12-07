3b31c430a14358e1c306b662ab703dc2d84c52a2
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-12-07 14:05:30 +0000

    cleanup

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,24 +1,13 @@
-import { create } from "@/actions/create-board";
-import { db } from "@/lib/db";
+import { Suspense } from "react";
+import { BoardList } from "./_components/board-list";
 
 const OrganizationIdPage = async () => {
-  const boards = await db.board.findMany();
-
   return (
-    <div className="flex flex-col space-y-4">
-      <form action={create}>
-        <input
-          id="title"
-          name="title"
-          required
-          placeholder="Enter a board title"
-          className="border-black border p-1"
-        />
-      </form>
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <div key={board.id}>Board name: {board.title}</div>
-        ))}
+    <div className="w-full mb-20">
+      <div className="px-2 md:px-4">
+        <Suspense>
+          <BoardList />
+        </Suspense>
       </div>
     </div>
   );
